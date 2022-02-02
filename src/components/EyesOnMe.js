8830a96e5db0183639fa2cc79ea69401b54b6c53
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-02 09:14:57 -0800

    commit tree 6adb14b46865337ea27fc5ccd3d9dfac6112035c

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,10 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function handleFocus() {console.log("Good!")}
+    function handleBlur() {console.log("Hey! Eyes on me!")}
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
